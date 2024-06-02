b2b2c0f08dd03054f67bb4605453d29533997eca
Daniel Wigh <dswigh@gmail.com>
2024-06-02 16:20:07 -0400

    remove space in png names

--- app.py
+++ app.py
@@ -192,12 +192,12 @@ st.write("___")
 # Case Study 1
 st.subheader("Case Study: Watershed Testing")
 st.write("In the Environmental Protection sector of the government we have been able to use the Chemical Toxicity Alert System in order to know the toxicity of both residential and commercial illicit discharge. This allowed us to input a list of chemicals released in these spills and see the toxicity of the chemicals. We then can know if the area needs to be pumped out. Understanding the toxicity of chemicals is one of the most important parts of the job so having software that requires little chemical experience allows us to complete our jobs more effectively.")
-st.image("figures/Guy Sampling.png")  # Placeholder image URL
+st.image("figures/Guy_Sampling.png")  # Placeholder image URL
 
 # Case Study 2
 st.subheader("Case Study: Reaction By-Products")
 st.write("There are many potential cases for the generation of unknown chemicals during an experiment. Running a new reaction, scaling up a reaction, or optimizing the reaction can all result in potentially toxic byproducts that can be difficult to test or product. By separating an impure mixture via LC-MS/GC-MS into fragments, the Chemical Toxicity Alert System (CTAS) can detect the compounds that have the highest potential to be toxic and classify their toxicity from the generated list of impurities, allowing for rapid detection of potentially toxic byproducts.This AI-powered technology simplifies laboratory safety protocols and is a vital tool for any chemist looking to develop greener methods. ")
-st.image("figures/mass spec.png")  
+st.image("figures/mass_spec.png")  
 
 # # Optionally, visualize the molecules using rdkit
 # st.subheader("Molecule Visualizations")
